32ddb34a58ce23471d7b5bccfa8f726d5c8f42b2
Shahzadforyou <zaeimshahzad@gmail.com>
2026-01-12 11:11:59 +0500

    setting conditions with elif

--- main.py
+++ main.py
@@ -7,3 +7,28 @@ you_dict = {
     "d" : -1
 
 }
+
+you = str(input("Enter \'a\' or \'s\'or \'d\'"))
+you_value = you_dict[you]
+
+if(you == 'a' or you == 's' or you == 'd' ):
+    if(you_value == 1 and computer == 1):
+        print("You win")
+    elif(you_value == 1 and computer == 0):
+        print("You lose")
+    elif(you_value == 1 and computer == -1):
+        print("You lose")
+    elif(you_value == 0 and computer == 1):
+        print("You lose")
+    elif(you_value == 0 and computer == 0):
+        print("You win")
+    elif(you_value == 0 and computer == -1):
+        print("You lose")
+    elif(you_value == -1 and computer == 1):
+        print("You lose")
+    elif(you_value == -1 and computer == 0):
+        print("You lose")
+    elif(you_value == -1 and computer == -1):
+        print("You win")
+else:
+    print("Enter valid value")
